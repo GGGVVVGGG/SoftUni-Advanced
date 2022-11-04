9661b3bee465ea5caeec73b25cf8114c6237661f
GGGVVVGGG <103410115+GGGVVVGGG@users.noreply.github.com>
2022-11-04 15:18:15 +0200

    -

--- Exams/Python Advanced Retake Exam - 15 December 2021/03_Naughty_or_Nice.py
+++ Exams/Python Advanced Retake Exam - 15 December 2021/03_Naughty_or_Nice.py
@@ -1,27 +1,69 @@
-def naughty_or_nice_list(*args, **kwargs):
-    result = {"Nice": [], "Naughty": [], "Not found": []}
-    output, list_with_names = "", args[0]
-    for data in args[1:]:
-        number, type_ = data.split("-")
-        total_numbers = 0
-        if sum(1 for num, name in list_with_names if int(number) == num) == 1:
-            for pos, (num, name) in enumerate(list_with_names):
-                if int(number) == num:
-                    total_numbers += 1
-                    result[type_].append(name)
-                    del list_with_names[pos]
-    for k_name, v_type in kwargs.items():
-        if sum(1 for num, name in list_with_names if k_name == name) == 1:
-            for pos, (num, name) in enumerate(list_with_names):
-                if k_name == name:
-                    del list_with_names[pos]
-                    result[v_type].append(name)
+def naughty_or_nice_list(kids, *args, **kwargs):
+    result_list = {"Nice": [], "Naughty": [], "Not found": []}
+    kid_numbers = [x[0] for x in kids]
+    for command in args:
+        num, type_ = [int(i) if i.isdigit() else i for i in command.split("-")]
+        for kid in kids:
+            if num == int(kid[0]) and kid_numbers.count(num) == 1:
+                result_list[type_].append(kid[1])
+                kids.remove((num, kid[1]))
 
-    result["Not found"] = [not_found_name for _, not_found_name in list_with_names]
-    for p_type, p_names in result.items():
-        if p_names:
-            output += f"{p_type}: {', '.join(p_names)}\n"
+    kid_names = [x[1] for x in kids]
+    for name, type_ in kwargs.items():
+        for kid in kids:
+            if name == kid[1] and kid_names.count(name) == 1:
+                result_list[type_].append(kid[1])
+                kids.remove(kid)
+    if kids:
+        [result_list["Not found"].append(kid[1]) for kid in kids]
+    result = [f'{k}: {", ".join(v)}' for k, v in result_list.items() if v]
+    return "\n".join(result)
 
-    return output
-
-# credits to CEO
+# Test Cases
+# print(naughty_or_nice_list(
+#     [
+#         (3, "Amy"),
+#         (1, "Tom"),
+#         (7, "George"),
+#         (3, "Katy"),
+#     ],
+#     "3-Nice",
+#     "1-Naughty",
+#     Amy="Nice",
+#     Katy="Naughty",
+# ))
+# print("=====================")
+# print()
+# print(naughty_or_nice_list(
+#     [
+#         (7, "Peter"),
+#         (1, "Lilly"),
+#         (2, "Peter"),
+#         (12, "Peter"),
+#         (3, "Simon"),
+#     ],
+#     "3-Nice",
+#     "5-Naughty",
+#     "2-Nice",
+#     "1-Nice",
+#     ))
+# print("=====================")
+# print()
+# print(naughty_or_nice_list(
+#     [
+#         (6, "John"),
+#         (4, "Karen"),
+#         (2, "Tim"),
+#         (1, "Merry"),
+#         (6, "Frank"),
+#     ],
+#     "6-Nice",
+#     "5-Naughty",
+#     "4-Nice",
+#     "3-Naughty",
+#     "2-Nice",
+#     "1-Naughty",
+#     Frank="Nice",
+#     Merry="Nice",
+#     John="Naughty",
+# ))
